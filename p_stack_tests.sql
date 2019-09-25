266dde13432074230b8f796768aab5b21c3c4267
Qualtagh <xgzaaa@gmail.com>
2019-09-25 23:29:17 +0500

    add initial tests for p_stack

---
+++ p_stack_tests.sql
@@ -0,0 +1,105 @@
+create or replace procedure assert_eq( tActual in varchar2, tExpected in varchar2 ) is
+  tExpectedReplaced varchar2( 4000 ) := replace( tExpected, '<USER>', user );
+begin
+  if tActual = tExpectedReplaced or tActual is null and tExpectedReplaced is null then
+    return;
+  end if;
+  raise_application_error( -20001, 'assertion failed: ' || chr( 10 ) || tActual || chr( 10 ) || tExpectedReplaced );
+end;
+/
+create or replace package APCKG is
+  procedure PROC;
+end;
+/
+create or replace package body APCKG is
+  procedure PROC is
+    procedure "INNER/proc" is
+    begin
+      assert_eq( p_stack.whoAmI, '5: <USER>.PACKAGE BODY APCKG.PROCEDURE PROC.PROCEDURE "INNER/proc"' );
+      assert_eq( p_stack.getSubprogram( p_stack.whoAmI ), 'INNER/proc' );
+      assert_eq( p_stack.getConcatenatedSubprograms( p_stack.whoAmI ), 'APCKG.PROC."INNER/proc"' );
+      assert_eq( p_stack.whoCalledMe, '11: <USER>.PACKAGE BODY APCKG.PROCEDURE PROC' );
+    end;
+  begin
+    "INNER/proc";
+  end;
+end;
+/
+begin
+  APCKG.PROC;
+end;
+/
+drop package APCKG;
+/
+declare
+  procedure inner_proc is
+  begin
+    assert_eq( p_stack.whoAmI, '4: <USER>.ANONYMOUS BLOCK.PROCEDURE INNER_PROC' );
+  end;
+begin
+  inner_proc;
+end;
+/
+declare
+  procedure outer_proc is
+    procedure inner_proc is
+      tCallStack varchar2( 4000 );
+      tCallLine varchar2( 4000 );
+    begin
+      tCallStack := p_stack.getCallStack;
+      assert_eq( p_stack.getDynamicDepth( tCallStack ), 4 );
+      
+      tCallLine := p_stack.getCallStackLine( tCallStack, 1 );
+      assert_eq( p_stack.getLexicalDepth( tCallLine ), 1 );
+      assert_eq( p_stack.getProgramType( tCallLine ), 'PACKAGE BODY' );
+      assert_eq( p_stack.getProgram( tCallLine ), 'P_STACK' );
+      assert_eq( p_stack.getSubprogramType( tCallLine ), 'FUNCTION' );
+      assert_eq( p_stack.getSubprogram( tCallLine ), 'GETCALLSTACK' );
+      assert_eq( p_stack.getConcatenatedSubprograms( tCallLine ), 'P_STACK.GETCALLSTACK' );
+      
+      tCallLine := p_stack.getCallStackLine( tCallStack, 2 );
+      assert_eq( p_stack.getUnitLine( tCallLine ), 7 );
+      assert_eq( p_stack.getOwner( tCallLine ), '<USER>' );
+      assert_eq( p_stack.getLexicalDepth( tCallLine ), 2 );
+      assert_eq( p_stack.getProgramType( tCallLine ), 'ANONYMOUS BLOCK' );
+      assert_eq( p_stack.getProgram( tCallLine ), '' );
+      assert_eq( p_stack.getSubprogramType( tCallLine ), 'PROCEDURE' );
+      assert_eq( p_stack.getSubprogram( tCallLine ), 'INNER_PROC' );
+      assert_eq( p_stack.getConcatenatedSubprograms( tCallLine ), 'ANONYMOUS BLOCK.OUTER_PROC.INNER_PROC' );
+      
+      tCallLine := p_stack.getCallStackLine( tCallStack, 3 );
+      assert_eq( p_stack.getOwner( tCallLine ), '<USER>' );
+      assert_eq( p_stack.getLexicalDepth( tCallLine ), 1 );
+      assert_eq( p_stack.getProgramType( tCallLine ), 'ANONYMOUS BLOCK' );
+      assert_eq( p_stack.getProgram( tCallLine ), '' );
+      assert_eq( p_stack.getSubprogramType( tCallLine ), 'PROCEDURE' );
+      assert_eq( p_stack.getSubprogram( tCallLine ), 'OUTER_PROC' );
+      assert_eq( p_stack.getConcatenatedSubprograms( tCallLine ), 'ANONYMOUS BLOCK.OUTER_PROC' );
+      
+      tCallLine := p_stack.getCallStackLine( tCallStack, 4 );
+      assert_eq( p_stack.getOwner( tCallLine ), '<USER>' );
+      assert_eq( p_stack.getLexicalDepth( tCallLine ), 0 );
+      assert_eq( p_stack.getProgramType( tCallLine ), 'ANONYMOUS BLOCK' );
+      assert_eq( p_stack.getProgram( tCallLine ), '' );
+      assert_eq( p_stack.getSubprogramType( tCallLine ), 'ANONYMOUS BLOCK' );
+      assert_eq( p_stack.getSubprogram( tCallLine ), '' );
+      assert_eq( p_stack.getConcatenatedSubprograms( tCallLine ), 'ANONYMOUS BLOCK' );
+      
+      tCallLine := p_stack.getCallStackLine( tCallStack, 5 );
+      assert_eq( p_stack.getUnitLine( tCallLine ), null );
+      assert_eq( p_stack.getOwner( tCallLine ), null );
+      assert_eq( p_stack.getLexicalDepth( tCallLine ), 0 );
+      assert_eq( p_stack.getProgramType( tCallLine ), 'PACKAGE' );
+      assert_eq( p_stack.getProgram( tCallLine ), null );
+      assert_eq( p_stack.getSubprogramType( tCallLine ), null );
+      assert_eq( p_stack.getSubprogram( tCallLine ), null );
+      assert_eq( p_stack.getConcatenatedSubprograms( tCallLine ), null );
+    end;
+  begin
+    inner_proc;
+  end;
+begin
+  outer_proc;
+end;
+/
+drop procedure assert_eq;
